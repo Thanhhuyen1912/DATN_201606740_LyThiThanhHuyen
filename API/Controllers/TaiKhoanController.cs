f2af983d44d18a3799f78306e7e377de20f8eeb3
Thanhhuyen1912 <huyenlythithanh55@gmail.com>
2025-03-23 22:51:39 +0700

    Add TaiKhoanController

---
+++ API/Controllers/TaiKhoanController.cs
@@ -0,0 +1,19 @@
+﻿using CoreLib.AppDbContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaiKhoanController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TaiKhoanController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+    }
+}
